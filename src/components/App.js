d892a2946142d6fbc70cd7b9afc9e43c241415df
Brian Bycynski <high5bri@hotmail.com>
2017-03-25 12:20:30 -0400

    css grid example

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return <div>
+      <h1>RESIZE THE WINDOW</h1>
       <div className='grid'>
         <div className='one'>Box 1</div>
         <div className='two'>Box 2</div>
@@ -10,6 +11,15 @@ class App extends Component {
         <div className='four'>Box 4</div>
         <div className='five'>Box 5</div>
         <div className='six'>Box 6</div>
+      </div>
+      <div className='grid1'>
+        <header>Header</header>
+        <article>
+          <h1>Article</h1>
+          <p>content for my article</p>
+        </article>
+        <aside>Aside</aside>
+        <footer>Footer</footer>
       </div>
     </div>
   }
